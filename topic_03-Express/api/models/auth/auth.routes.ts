840b40d6d53a120656a1b55f195772557286b698
Ivan Lazarevski <Ivan.Lazarevski@seavus.com>
2021-10-28 19:23:03 +0200

    code wrapped

--- topic_03-Express/api/models/auth/auth.routes.ts
+++ topic_03-Express/api/models/auth/auth.routes.ts
@@ -1,8 +1,11 @@
-import {Router} from 'express';
-import { AuthController } from './auth.controller';
+import { Router } from "express";
+import { AuthController } from "./auth.controller";
 
 const router = Router();
 
 router.post("/register", AuthController.registerNewUser);
+router.post("/login", AuthController.loginUser);
+router.post("/logout", AuthController.logoutUser);
+router.get("/current", AuthController.getCurrentUser);
 
 export default router;
